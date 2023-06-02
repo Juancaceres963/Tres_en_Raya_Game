66b02d3d9601c48b1fcadb04d537d555bd0f1b38
Juancaceres963 <dnijuancaceres@gmail.com>
2023-06-02 01:49:25 -0300

    Logica para revisar si existe un ganador

--- src/App.jsx
+++ src/App.jsx
@@ -20,18 +20,52 @@ const Square = ({ children, isSelected, updateBoard, index}) => {
   )
 }
 
+const WINNER_COMBOS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null))
 
   const [turn, setTurn] = useState(TURNS.X)
+  const [winner, setWinner] = useState(null) // Null no hay ganador y fall es que hay un empate
 
+  const checkWinner = (boardToCheck) => {
+    // Revisamos todas las combinaciones ganadoras a ver si 'x' o 'o' ganan
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo
+      if (boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+        ) {
+          return boardToCheck[a]
+        }
+    }
+    // Si no hay ganador 
+    return null
+  }
   const updateBoard = (index) => {
+    //No permitimos que sobre escriba si alguno ya utilizo esa cacilla
+    if (board[index] || winner) return
+    // Actualizamos el tablero 
     const newBord = [...board]
     newBord[index] = turn
     setBoard(newBord)
-
+    // Cambiamos el tablero
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+    // Revisamos si hay un ganador
+    const newWinner = checkWinner(newBord)
+    if (newWinner){
+      setWinner(newWinner)
+    }
   }
 
   return (
